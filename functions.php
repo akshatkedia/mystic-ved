4d2477d154394e0fbf5f68ca8e3190408e7afaa5
Akshat Kedia <akshat.kedia@outlook.com>
2017-10-29 19:35:47 +0530

    first commit

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+function mystic_ved_enqueue_styles() {
+  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+  wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ), null );
+}
+add_action( 'wp_enqueue_scripts', 'mystic_ved_enqueue_styles' );
